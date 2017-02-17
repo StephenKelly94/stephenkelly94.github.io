525d76cfe188a00bcadbc1923d3ef534d0bf6762
Stephen K <stephen.kelly.94@hotmail.com>
2017-02-17 00:10:28 +0000

    Updated headings and started about

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-window.onscroll = function(){
-  var windowHeight = document.documentElement.clientHeight - 70
-  if (document.body.scrollTop > windowHeight || document.documentElement.scrollTop > windowHeight) {
-    document.getElementById("hero-section").className = "site-nav nav-out-of-hero";
+
+$(document).ready(function(){
+  checkNavbar();
+  $(window).scroll(checkNavbar);
+
+  $("a").click(function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 70
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+function checkNavbar(){
+  var scrollTop = $(this).scrollTop()
+  if( scrollTop > 20 )
+  {
+    $('#hero-section').addClass('has-scrolled');
   }else{
-    document.getElementById("hero-section").className = "site-nav";
+    $('#hero-section').removeClass('has-scrolled');
   }
-};
+}
